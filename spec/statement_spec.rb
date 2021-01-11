6b0fbf473356d29bb032c9a4dcb6584f07161e31
alexleesonmill <alexleesonmill@gmail.com>
2021-01-11 18:15:07 +0000

    Tests - Removed two redundant tests and made printing methods private

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,14 +2,6 @@ describe Statement do
   let(:statement) { Statement.new }
 
   context 'Displaying Statement' do
-    it 'displays a proper header' do
-      expect{ statement.print_header }.to output('date || credit || debit || balance\n').to_stdout
-    end
-
-    it 'displays the first line of the statement correctly' do
-      expect{ statement.print_transactions }.to output('14/01/2012 || || 500.00 || 2500.00\n').to_stdout
-    end
-
     it 'displays a printed statement fully' do
       expect { statement.print_statement }.to output('date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00\n').to_stdout
     end
