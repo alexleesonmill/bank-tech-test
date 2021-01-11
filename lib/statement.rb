213f79c5d4a707c7d54fc128ddc3c913d696bbe8
alexleesonmill <alexleesonmill@gmail.com>
2021-01-11 18:07:44 +0000

    Model - Printouts for header and statement

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,9 +1,16 @@
 class Statement
+  def print_statement
+    print_header
+    print_transactions
+  end
+
+  private
+
   def print_header
     print 'date || credit || debit || balance\n'
   end
 
   def print_transactions
-    print '14/01/2012 || || 500.00 || 2500.00'
+    print '14/01/2012 || || 500.00 || 2500.00\n'
   end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,12 +1,17 @@
 describe Statement do
   let(:statement) { Statement.new }
+
   context 'Displaying Statement' do
     it 'displays a proper header' do
       expect{ statement.print_header }.to output('date || credit || debit || balance\n').to_stdout
     end
 
     it 'displays the first line of the statement correctly' do
-      expect{ statement.print_transactions }.to output('14/01/2012 || || 500.00 || 2500.00').to_stdout
+      expect{ statement.print_transactions }.to output('14/01/2012 || || 500.00 || 2500.00\n').to_stdout
+    end
+
+    it 'displays a printed statement fully' do
+      expect { statement.print_statement }.to output('date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00\n').to_stdout
     end
   end
 end
