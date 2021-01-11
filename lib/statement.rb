c94cc410e4569e3600459ecfa46b623b1dfdad1a
alexleesonmill <alexleesonmill@gmail.com>
2021-01-11 18:04:52 +0000

    Test - Printing Transactions hardcoded

--- lib/statement.rb
+++ lib/statement.rb
@@ -2,4 +2,8 @@ class Statement
   def print_header
     print 'date || credit || debit || balance\n'
   end
+
+  def print_transactions
+    print '14/01/2012 || || 500.00 || 2500.00'
+  end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,5 +4,9 @@ describe Statement do
     it 'displays a proper header' do
       expect{ statement.print_header }.to output('date || credit || debit || balance\n').to_stdout
     end
+
+    it 'displays the first line of the statement correctly' do
+      expect{ statement.print_transactions }.to output('14/01/2012 || || 500.00 || 2500.00').to_stdout
+    end
   end
 end
