fab3a18bb79d7365425adbfb7a187e2b4000d1b2
alexleesonmill <alexleesonmill@gmail.com>
2021-01-11 18:01:51 +0000

    Test - Displaying test header

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,3 +1,5 @@
 class Statement
-
+  def print_header
+    print 'date || credit || debit || balance\n'
+  end
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,3 +1,8 @@
 describe Statement do
-
+  let(:statement) { Statement.new }
+  context 'Displaying Statement' do
+    it 'displays a proper header' do
+      expect{ statement.print_header }.to output('date || credit || debit || balance\n').to_stdout
+    end
+  end
 end
