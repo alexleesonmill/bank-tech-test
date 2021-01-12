f1a41bbb4f14738c9bd02d6c2a56f757884295e7
alexleesonmill <alexleesonmill@gmail.com>
2021-01-12 10:36:50 +0000

    Tests - Doubles set up for statement testing

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,12 +2,14 @@
 
 describe Statement do
   let(:statement) { Statement.new }
+  let(:transactions) { [transaction_1, transaction_2] }
+  let(:transaction_1) { double(credit: 50, debit: 0, current_balance: 50, date: 12/01/2021) }
+  let(:transaction_2) { double(credit: 0, debit: 20, current_balance: 30, date: 12/01/2021) }
 
   context 'Displaying Statement' do
     it 'displays a printed statement fully' do
-      expect { statement.print_statement } .to output('date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00\n').to_stdout
+      expect { statement.print_statement(transactions) } .to output('date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00\n').to_stdout
     end
-
 
   end
 end
